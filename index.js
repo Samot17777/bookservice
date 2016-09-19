3f57232ac7cacd3b238add2a6c1f8edc1e3808b7
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-09-19 10:29:28 +0200

    echo server with body parser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 function logIncoming(req, res, next) {
     console.log("incoming request " + new Date());
@@ -23,8 +26,11 @@ function serverError(err, req, res, next) {
 }
 
 app.get('/', logIncoming, function (req, res) {
-    throw new Error("ASDfsdaf");
     res.send('Hello World');
+});
+
+app.post('/stock', function (req, res) {
+    res.json({isbn: req.body.isbn, count: req.body.count})
 });
 
 app.use(clientError);
