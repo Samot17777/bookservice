4a5e1f7717cecac2dd3f8d56a0bc51ef7760971c
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-09-19 13:35:07 +0200

    introducing stock repository abstraction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var stockRepository = require('./stockRepository');
 
 app.use(bodyParser.json());
 
@@ -28,42 +28,22 @@ function serverError(err, req, res, next) {
 
 var url = 'mongodb://localhost:27017/book_inventory_service';
 
-var collection = MongoClient.connect(url, {db: {bufferMaxEntries: 0}}).then(function (db) {
-    return db.collection('books');
-});
-//MongoClient.connect(url, function (err, db) {
-//    if(err) {
-//        console.error(err);
-//        process.exit(1);
-//    }
-//    else {
-//        console.log("Connected succesfully to server");
-//
-//        setTimeout(function() {
-//            collection = db.collection('books');
-//        }, 10000);
-//    }
-//});
-
-
 app.post('/stock', function (req, res, next) {
-    collection.then(function (collection) {
-        return collection.updateOne({isbn: req.body.isbn}, {
-            isbn: req.body.isbn,
-            count: req.body.count
-        }, {upsert: true});
-    }).then(function () {
-        res.json({isbn: req.body.isbn, count: req.body.count});
-    }).catch(next);
-
+    stockRepository.
+        stockUp(req.body.isbn, req.body.count).
+        then(function () {
+            res.json({isbn: req.body.isbn, count: req.body.count});
+        }).
+        catch(next);
 });
 
 app.get('/stock', function (req, res, next) {
-    collection.then(function (collection) {
-        return collection.find({}).toArray();
-    }).then(function (result) {
-        res.json(result);
-    }).catch(next);
+    stockRepository.
+        findAll().
+        then(function (results) {
+            res.json(results);
+        }).
+        catch(next);
 });
 
 app.use(clientError);
