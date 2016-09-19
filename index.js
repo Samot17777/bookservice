e6380d1107e50f4115eb41abef0e2aa570b7a500
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-09-19 10:14:48 +0200

    error handling

--- index.js
+++ index.js
@@ -6,9 +6,29 @@ function logIncoming(req, res, next) {
     next();
 }
 
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function serverError(err, req, res, next) {
+    res.status(err.status || 500);
+    console.error(err.stack);
+    res.json({
+            message: err.message,
+            error: (process.env.NODE_ENV === 'production') ? {} : err.stack
+        }
+    );
+}
+
 app.get('/', logIncoming, function (req, res) {
+    throw new Error("ASDfsdaf");
     res.send('Hello World');
 });
+
+app.use(clientError);
+app.use(serverError);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
