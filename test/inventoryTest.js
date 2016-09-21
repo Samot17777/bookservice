12e06638cb45dfc9d66f4fb2190c0175c6aa9e48
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-21 11:19:26 +0200

    ss

--- test/inventoryTest.js
+++ test/inventoryTest.js
@@ -7,7 +7,6 @@ it('allows to stock up the items', function (done) {
     request(app).
         post('/stock').
         send({isbn: '1234', count: 10}).
-        expect('Content-Type', /json/).
         expect(200, {isbn: '1234', count: 10}, done);
 });
 
