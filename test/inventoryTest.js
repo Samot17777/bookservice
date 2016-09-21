76c3b92b758131979e811134eb996e102aad77c2
Tomasz Wylężek <Tomasz.Wylezek@jcommerce.pl>
2016-09-21 11:24:06 +0200

    ss

--- test/inventoryTest.js
+++ test/inventoryTest.js
@@ -2,11 +2,12 @@ var request = require('supertest');
 var assert = require('assert');
 
 it('allows to stock up the items', function (done) {
-    var app = require('../lib/app.js')(require('../lib/inMemoryStockRepository')());
+    var app = require('../lib/app.js')(require('../lib/inMemoryStockRepository')(),true);
 
     request(app).
         post('/stock').
         send({isbn: '1234', count: 10}).
+        expect('Content-Type', /json/).
         expect(200, {isbn: '1234', count: 10}, done);
 });
 
